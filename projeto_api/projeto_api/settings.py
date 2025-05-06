e5d20f29bb4528be43dd3c5aaf1d8d3c781d5a35
CamillyBenato <camillybenato.dev2024@gmail.com>
2025-05-06 15:49:49 -0300

    Projeto

--- projeto_api/projeto_api/settings.py
+++ projeto_api/projeto_api/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS =  [
+    "http://localhost:5500",
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
