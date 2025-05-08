901ccd7b8fdfeb2d79885e25646c1ec3203d96be
CamillyBenato <camillybenato.dev2024@gmail.com>
2025-05-08 15:56:36 -0300

    Projeto

--- frontend/script.js
+++ frontend/script.js
@@ -4,13 +4,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function fetchProdutos(){
     fetch("http://localhost:8000/api/produtos/")
-    .then(res => res.json())
-    .then(data => renderProdutos(data))
-    .catch(err => console.error("Erro ao buscar Produtos", err));
+        .then(res => res.json())
+        .then(data => {
+            renderCarrossel(data);
+            renderTodosProdutos(data);
+        })
+        .catch(err => console.error("Erro ao buscar Produtos", err));
 }
 
-function renderProdutos(produtos){
-
+//Carrossel
+function renderCarrossel(produtos){
     const container = document.getElementById("produtos-container");
     container.innerHTML = "";
 
@@ -18,14 +21,33 @@ function renderProdutos(produtos){
         const card = document.createElement("div");
         card.className = "produto";
         card.innerHTML = `
-            <div class = "imagem-container">
-            <a href = './descricao.html?id=${produto.id}'>
-                <img src = "${produto.imagem}" alt = "${produto.nome}"/>
+            <div class="imagem-container">
+                <a href="./descricao.html?id=${produto.id}">
+                    <img src="${produto.imagem}" alt="${produto.nome}" />
                 </a>
             </div>
-            
         `;
         container.appendChild(card);
     });
-    
+}
+
+//Todos os Produtos
+function renderTodosProdutos(produtos){
+    const todosContainer = document.getElementById("produtos-todos");
+    todosContainer.innerHTML = "";
+
+    produtos.forEach(produto => {
+        const card = document.createElement("div");
+        card.className = "produto-item";
+        
+        card.innerHTML = `
+            <div class="produto-completo">
+                <img src="${produto.imagem}" alt="${produto.nome}" />
+                <h2>${produto.nome}</h2>
+                <p class="preco">R$ ${produto.preco}</p>
+                <p><strong>Categoria:</strong> ${produto.categoria.nome}</p>
+            </div>
+        `;
+        todosContainer.appendChild(card);
+    });
 }
