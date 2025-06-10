5889a926b52aeab440e0ffa366d27c3ec81a6f1d
otavio-mmulato <otavio.mmulato@outlook.com.br>
2025-06-09 21:38:34 -0300

    ajuste no erro de senha não indo pro admin + mudança do http response para alert

--- jardim_da_diana/usuarios/views.py
+++ jardim_da_diana/usuarios/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login as login_django
 from django.contrib.auth.decorators import login_required
@@ -10,29 +9,29 @@ def cadastro(request):
     else:
         username = request.POST.get('username')
         email = request.POST.get('email')
-        senha = request.POST.get('password')
+        senha = request.POST.get('senha')
 
         if User.objects.filter(username=username).exists():
-            return HttpResponse('Já existe um usuário com esse username')
+            return render(request, 'cadastro.html', {'erro': 'Já existe um usuário com esse username.'})
 
         User.objects.create_user(username=username, email=email, password=senha)
-        return redirect('/auth/login/')  # Redireciona após cadastro
+        return redirect('/auth/login/')
 
 def login(request):
     if request.method == 'GET':
         return render(request, 'login.html')
     else:
         username = request.POST.get('username')
-        senha = request.POST.get('password')
+        senha = request.POST.get('senha')
 
         user = authenticate(request, username=username, password=senha)
 
         if user:
             login_django(request, user)
-            return redirect('/static/frontend/index.html')  # Redireciona para a home
+            return redirect('/static/frontend/index.html')
         else:
-            return HttpResponse('Usuário ou senha inválidos')  # Mantém mensagem de erro
+            return render(request, 'login.html', {'erro': 'Usuário ou senha inválidos.'})
 
 @login_required(login_url='/auth/login/')
 def plataforma(request):
-    return HttpResponse('Bem-vindo à plataforma! Você está logado.')
+    return render(request, 'plataforma.html')
